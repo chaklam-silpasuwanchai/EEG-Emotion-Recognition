08f44f11125fa8d5d597ef76d22afd619b26c66e
Akraradet Sinsamersuk <akraradets@gmail.com>
2022-05-26 17:15:54 +0000

    before run the script

--- projects/components/ml.py
+++ projects/components/ml.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedGroupKFold,GridSearchCV
+from sklearn.model_selection import StratifiedGroupKFold,GridSearchCV,StratifiedKFold
 from sklearn.svm import SVC
 from IPython.display import display
 import pandas as pd
 import os
 from typing import Tuple
+import logging
 
 def train_model_segment_first(X,y,groups,cv_result_prefix="") -> np.ndarray:
     """
@@ -19,6 +20,7 @@ def train_model_segment_first(X,y,groups,cv_result_prefix="") -> np.ndarray:
     # If we do "Split-first", then models learn little record of every trial
     # If we doe "Segment-first", then models learn 90% of trials and test on 10%
     # This split-first can be done using "group" argument during split.
+    logging.debug(f"Running Segment-First")
     n_split_outter = 10
     cv_outter = StratifiedGroupKFold(n_splits=n_split_outter, shuffle=False)
     accs = []
@@ -29,7 +31,7 @@ def train_model_segment_first(X,y,groups,cv_result_prefix="") -> np.ndarray:
         groups_train, groups_test = groups[idxs_train], groups[idxs_test]
         assert set(groups_train).isdisjoint(set(groups_test)),f"Contaminated.\ngroups_train:{groups_train}\ngroups_test:{groups_test}"
 
-        grid = build_model(X_train,y_train,groups_train)
+        grid = build_model_with_group(X_train,y_train,groups_train)
         # Evaluation
         model = grid.best_estimator_
         predict = model.predict(X_test) # type: ignore
@@ -39,10 +41,11 @@ def train_model_segment_first(X,y,groups,cv_result_prefix="") -> np.ndarray:
         pd.DataFrame(grid.cv_results_).to_csv(f"{cv_result_prefix}-{epoch+1}.csv")
     return np.array(accs)
 
-def build_model(X,y,groups) -> GridSearchCV:
+def build_model_with_group(X,y,groups) -> GridSearchCV:
     """
         This function will only optimized models
     """
+    logging.debug(f"Running build_model_with_group")
     n_split = 9
     cv = StratifiedGroupKFold(n_splits=n_split, shuffle=True, random_state=42)
     # https://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html
@@ -52,8 +55,60 @@ def build_model(X,y,groups) -> GridSearchCV:
             {"kernel": ["linear"], "C": C_range},
             {"kernel": ["rbf"],    "C": C_range, "gamma": gamma_range},
         ]
-    grid = GridSearchCV(SVC(), param_grid=tuned_parameters, cv=cv, n_jobs=os.cpu_count(), refit=True, verbose=1, return_train_score=True)
+    grid = GridSearchCV(SVC(), param_grid=tuned_parameters, cv=cv, n_jobs=os.cpu_count(), refit=True, verbose=4, return_train_score=True)
     grid.fit(X=X, y=y, groups=groups)
+    # df = pd.DataFrame(grid.cv_results_)
+    # return grid.best_estimator_, df
+    return grid
+
+
+def train_model_split_first(X,y,groups,cv_result_prefix="") -> np.ndarray:
+    """
+        X: The sample set with shape of (n_samples, n_features)
+        y: The labels with shape of (n_samples,)
+        groups: will be ignored
+    """
+    # Here we have to perform double cross validation.
+    # The idea is we will do 10-CV
+    # The option we have to choose is split-first or segment-first
+    # If we do "Split-first", then models learn little record of every trial
+    # If we doe "Segment-first", then models learn 90% of trials and test on 10%
+    # This split-first can be done using "group" argument during split.
+    logging.debug(f"Running Split-First")
+    n_split_outter = 10
+    cv_outter = StratifiedKFold(n_splits=n_split_outter, shuffle=False)
+    accs = []
+    for epoch, (idxs_train, idxs_test) in enumerate(cv_outter.split(X,y)):
+        print(f"BEGIN EPOCH: {epoch+1}/{n_split_outter}")
+        X_train, X_test = X[idxs_train], X[idxs_test]
+        y_train, y_test = y[idxs_train], y[idxs_test]
+
+        grid = build_model(X_train,y_train)
+        # Evaluation
+        model = grid.best_estimator_
+        predict = model.predict(X_test) # type: ignore
+        acc = sum(predict == y_test) / len(y_test)
+        accs.append(acc)
+        # save csv
+        pd.DataFrame(grid.cv_results_).to_csv(f"{cv_result_prefix}-{epoch+1}.csv")
+    return np.array(accs)
+
+def build_model(X,y) -> GridSearchCV:
+    """
+        This function will only optimized models
+    """
+    logging.debug(f"Running build_model")
+    n_split = 9
+    cv = StratifiedKFold(n_splits=n_split, shuffle=True, random_state=42)
+    # https://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html
+    C_range = np.logspace(-2, 10, 13)
+    gamma_range = np.logspace(-9, 3, 13)
+    tuned_parameters = [
+            {"kernel": ["linear"], "C": C_range},
+            {"kernel": ["rbf"],    "C": C_range, "gamma": gamma_range},
+        ]
+    grid = GridSearchCV(SVC(), param_grid=tuned_parameters, cv=cv, n_jobs=os.cpu_count(), refit=True, verbose=4, return_train_score=True)
+    grid.fit(X=X, y=y)
     # df = pd.DataFrame(grid.cv_results_)
     # return grid.best_estimator_, df
     return grid
--- projects/main-ml.py
+++ projects/main-ml.py
@@ -1,11 +1,12 @@
 from curses import meta
 from time import time
 from components.dataset_jo import Dataset_subjectDependent as MyDataset
-from components.ml import train_model_segment_first
+from components.ml import train_model_segment_first, train_model_split_first
 from components.preprocessing import DE
 import os
 import logging
 import argparse
+import numpy as np
 
 def get_argument() -> argparse.Namespace:
     # python filename -h description
@@ -29,6 +30,8 @@ if __name__ == '__main__':
                     datefmt='%d-%m-%Y %H:%M:%S',
                     level=logging.INFO)
     logging.info(f'''
+    Running Command: 
+        python3 main-ml.py --subject_setup {args.subject_setup} --experimental_setup {args.experimental_setup} --stimuli_class {args.stimuli_class} --preprocessing {args.preprocessing} --segment_lenght {args.segment_lenght} --output_log {args.output_log}
     Starting training:
         segment_lenght:  {args.segment_lenght}
         output_log:      {args.output_log}
@@ -37,30 +40,80 @@ if __name__ == '__main__':
         subject_setup:   {args.subject_setup}
         experimental_setup: {args.experimental_setup}
     ''')
+    # Check input parameter
+    assert args.preprocessing in preprocessing_option,          f"The preprocessing '{args.preprocessing}' is not supported."
+    assert args.stimuli_class in ['valence','arousal'],         f"The stimuli_class '{args.stimuli_class}' is not supported."
+    assert args.subject_setup in ['dependent', 'independent'],  f"The subject_setip '{args.subject_setup}' is not supported."
+    assert args.experimental_setup in ['trial', 'segment'],     f"The experimental_setup '{args.experimental_setup}' is not supported."
+
     # Load dataset from path ./data. Inside the path must be s01, s02, s03, ...
     # Lazyload mean the class will not load data if not use. This will save some RAM. 
     # But it will eventually load all the data because I did not write garbage collector
     dataset = MyDataset(dataset_path='data', lazyload=True)
     dataset.set_segment(7680//(128*args.segment_lenght))
-    assert args.preprocessing in preprocessing_option,          f"The preprocessing '{args.preprocessing}' is not supported."
-    assert args.stimuli_class in ['valence','arousal'],         f"The stimuli_class '{args.stimuli_class}' is not supported."
-    assert args.subject_setup in ['dependent', 'independent'],  f"The subject_setip '{args.subject_setup}' is not supported."
-    assert args.experimental_setup in ['trial', 'segment'],     f"The experimental_setup '{args.experimental_setup}' is not supported."
+
     stimuli_class = 0
     if(args.stimuli_class == 'valence'):
         stimuli_class = MyDataset.STIMULI_VALENCE
     elif(args.stimuli_class == 'arousal'):
         stimuli_class = MyDataset.STIMULI_AROUSAL
+
+    # init output folder
     output_gridsearch_path = f"./output/gridSearch-{args.subject_setup}-{args.experimental_setup}-{args.stimuli_class}-{args.preprocessing}-{args.segment_lenght}"
     if(os.path.exists(output_gridsearch_path) == False):
         os.mkdir(output_gridsearch_path)
 
-    for filename in dataset.get_file_list():
-        start = time()
-        data, labels, groups = dataset.get_data(filename, stimuli=stimuli_class, return_type='numpy')
-        X = None
-        if(args.preprocessing == 'DE'):
-            X = DE(data)
-        cv_scores = train_model_segment_first(X, labels.reshape(-1), groups, cv_result_prefix=f"{output_gridsearch_path}/{filename}")
-        logging.info(f"AROUSAL-{filename}|10-CV={format(  round(cv_scores.mean(),5), '.5f')}|STD={format(  round(cv_scores.std(),5), '.5f')}|Time spend={time() - start}")
+    # assign preprocessing
+    preprocessing = None
+    if(args.preprocessing == 'DE'):
+        preprocessing = DE
+    experimental_setup = None
+    if(args.experimental_setup == 'trial'):
+        experimental_setup = train_model_segment_first
+    elif(args.experimental_setup == 'segment'):
+        experimental_setup = train_model_split_first
 
+    cv_scores_final, cv_scores_std_final = [], []
+    if(args.subject_setup == 'dependent'):
+        for filename in dataset.get_file_list():
+            start = time()
+            data, labels, groups = dataset.get_data(filename, stimuli=stimuli_class, return_type='numpy')
+
+            X = preprocessing(data)
+            # if experimental_setup is split_first, groups will be ignored
+            cv_scores = experimental_setup(X, labels.reshape(-1), groups, cv_result_prefix=f"{output_gridsearch_path}/{filename}")
+
+            logging.info(f"{filename}|10-CV={format(  round(cv_scores.mean(),5), '.5f')}|STD={format(  round(cv_scores.std(),5), '.5f')}|Time spend={time() - start}")
+            cv_scores_final.append(cv_scores.mean())
+            cv_scores_std_final.append(cv_scores.std())
+    elif(args.subject_setup == 'independent'):
+        # Get all subj in one big datas, labels, groups
+        all_datas, all_labels, all_groups = [],[],[]
+        for filename in dataset.get_file_list():
+            data, labels, groups = dataset.get_data(filename, stimuli=0, return_type='numpy')
+            # subj 1 will have groups start from 100 101 102 ... 139
+            # subj 2 will have groups start from 200 201 202 ... 239
+            # ...
+            # subj 32 will have groups start from 3200 3201 3202 ... 3239
+            groups = int(filename[1:])*100 +  groups
+            # print(filename, int(filename[1:])*100 +  groups)
+            all_datas.append(data)
+            all_labels.append(labels)
+            all_groups.append(groups.reshape(-1,1))
+        all_datas = np.vstack(all_datas)
+        all_labels = np.vstack(all_labels).reshape(-1)
+        all_groups = np.vstack(all_groups).reshape(-1)
+
+        start = time()
+        X = preprocessing(all_datas)
+        # if experimental_setup is split_first, groups will be ignored
+        cv_scores = experimental_setup(X, all_labels.reshape(-1), all_groups, cv_result_prefix=f"{output_gridsearch_path}/{filename}")
+
+        logging.info(f"ALL|10-CV={format(  round(cv_scores.mean(),5), '.5f')}|STD={format(  round(cv_scores.std(),5), '.5f')}|Time spend={time() - start}")
+        cv_scores_final.append(cv_scores.mean())
+        cv_scores_std_final.append(cv_scores.std())
+
+
+    cv_scores_final = np.array(cv_scores_final)
+    cv_scores_std_final = np.array(cv_scores_std_final)
+    logging.info(f"final|10-CV={format(  round(cv_scores_final.mean(),5), '.5f')}|STD={format(  round(cv_scores_std_final.std(),5), '.5f')}")
